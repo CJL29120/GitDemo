599fed4d6b1e7a151e44dfe569a47100730898c4
29120 <291203136@qq.com>
2021-04-11 17:10:34 +0800

    使用dev分支提交修改文件 （FunctionForChen.java，README.md）

--- module-a/src/main/java/FunctionForChen.java
+++ module-a/src/main/java/FunctionForChen.java
@@ -1,7 +1,13 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FunctionForChen {
 
     @Override
     public String toString() {
+
+
+
         return "FunctionForChen ";
     }
 }
