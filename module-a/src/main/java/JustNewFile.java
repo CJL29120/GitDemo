2ec742311b6ac63d7034a414a307617735f44793
29120 <291203136@qq.com>
2021-04-11 11:10:32 +0800

    main 提交

--- module-a/src/main/java/JustNewFile.java
+++ module-a/src/main/java/JustNewFile.java
@@ -1,2 +1,12 @@
 public class JustNewFile {
+    public static void main(String[] args) {
+        //todo main分支 模拟修改代码
+    }
+
+
+
+
+    private String getS(){
+        return "Test move to another changelist";
+    }
 }
